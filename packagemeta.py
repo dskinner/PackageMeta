bda55fbc9dc206d49a22b934642646056792d5a8
Daniel Skinner <daniel@dasa.cc>
2012-12-08 12:21:58 -0600

    updates for handling as import or sublime package window commands for interacting with plugin without import std python formatting, docstrings, license

--- packagemeta.py
+++ packagemeta.py
@@ -1,8 +1,31 @@
 """
+Copyright (c) 2012, Daniel Skinner <daniel@dasa.cc>
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 import logging
 import threading
 import os
+import sys
 
 import sublime
 import sublime_plugin
@@ -11,54 +34,64 @@ PackageControl = __import__("Package Control")
 
 
 def logger(level):
-	sh = logging.StreamHandler()
-	sh.setLevel(logging.DEBUG)
-	sh.setFormatter(logging.Formatter('%(levelname)s:%(name)s:%(message)s'))
-	log = logging.getLogger("PackageMeta")
-	log.setLevel(level)
-	log.addHandler(sh)
-	return log
+    sh = logging.StreamHandler()
+    sh.setLevel(logging.DEBUG)
+    sh.setFormatter(logging.Formatter('%(levelname)s:%(name)s:%(message)s'))
+    log = logging.getLogger("PackageMeta")
+    log.setLevel(level)
+    log.addHandler(sh)
+    return log
 
 
 log = logger(logging.DEBUG)
+
+
+def _add_to_path():
+    log.debug("module name is %s", __name__)
+    path = os.path.join(sublime.packages_path(), "__PackageMeta")
+    if path not in sys.path:
+        log.debug("adding sys path: %s", path)
+        sys.path.append(path)
+_add_to_path()
+
 
 _requires = {}
 _receivers = {}
 
 
 class Receiver(object):
-	"""Base class for receiving broadcast data.
+    """Base class for receiving broadcast data.
 
-	Simply subclassing this class will instantiate and register the class for receiving
-	broadcast data. For example, to receive data from a broadcast given the name "lint_java",
-	the following is all that is needed:
+    Subclassing this will automatically instantiate and register the class for receiving
+    broadcast data. For example, to receive data from a broadcast given the name "lint_java",
+    the following is all that is needed:
 
-	class LintJavaReceiver(packagemeta.Receiver):
-		name = "lint_java"
+    class LintJavaReceiver(packagemeta.Receiver):
+        channel = "lint_java"
 
-		def receive(self, data):
-			pass  # handle data
+        def receive(self, data):
+            pass  # handle data
 
-	The type of data received is determined by the broadcaster. Multiple receivers can watch
-	the same channel and all receivers will be notified.
+    The type of data received is determined by the broadcaster. Multiple receivers can watch
+    the same channel and all receivers will be notified.
 
-	Attributes:
-		channel: key used to register a subclass for receiving broadcast data by the same name.
-	"""
-	channel = None
+    Attributes:
+        channel: key used to register a subclass for receiving broadcast data by the same name.
+    """
+    channel = None
 
-	def receive(self, data):
-		pass
+    def receive(self, data):
+        pass
 
 
 def _register_receivers():
-	"""Find all subclasses of Receiver and register them for receiving broadcast data"""
-	subs = Receiver.__subclasses__()
-	for sub in subs:
-		if sub.channel is None:
-			log.warn("Receiver %s failed to define `channel` member.", sub)
-			continue
-		_receivers[sub.channel] = _receivers.get(sub.channel, []) + [sub()]
+    """Find all subclasses of Receiver and register them for receiving broadcast data"""
+    subs = Receiver.__subclasses__()
+    for sub in subs:
+        if sub.channel is None:
+            log.warn("Receiver %s failed to define `channel` member.", sub)
+            continue
+        _receivers[sub.channel] = _receivers.get(sub.channel, []) + [sub()]
 
 
 # TODO could be better
@@ -66,105 +99,166 @@ sublime.set_timeout(_register_receivers, 3000)
 
 
 def broadcast(channel, data):
-	"""Broadcast data on a given channel.
-	"""
-	if not isinstance(channel, (str, unicode)):
-		raise Exception("")
+    """Broadcast data on a given channel.
+    """
+    if not isinstance(channel, (str, unicode)):
+        raise Exception("")
 
-	def _broadcast():
-		for receiver in _receivers.get(channel, []):
-			receiver.receive(data)
+    def _broadcast():
+        for receiver in _receivers.get(channel, []):
+            receiver.receive(data)
 
-	threading.Thread(target=_broadcast).start()
+    threading.Thread(target=_broadcast).start()
 
 
 def exists(*pkgs):
-	for pkg in pkgs:
-		if not os.path.exists(os.path.join(sublime.packages_path(), pkg)):
-			return False
-	return True
+    for pkg in pkgs:
+        if not os.path.exists(os.path.join(sublime.packages_path(), pkg)):
+            return False
+    return True
 
 
 def requires(*pkgs):
-	def _decor(fn):
-		global _requires
-		s = _requires.get(fn.__module__, set())
-		for pkg in pkgs:
-			s.add(pkg)
-		_requires[fn.__module__] = s
+    """Decor for registering external dependencies within a module.
 
-		def _require(*args, **kwargs):
-			if exists(pkg):
-				return fn(*args, **kwargs)
-		return _require
-	return _decor
+    Use of this decor should be constrained to the module level if poossible.
+    When used, this registers the original module to be associated with a
+    package dependency for the associated function. If the package is not
+    available, then the function is not run, and `None` will be returned. For
+    example:
+
+    @packagemeta.requires("ExternalPackage")
+    def configure_externalpackage(settings):
+        settings.set("externalpackage_setting", True)
+
+    Since the package is associated with the module, a quick panel command
+    is also available for installing all dependencies via PackageControl.
+    See `packagemeta.InstallRequires` for more info.
+    """
+    def _decor(fn):
+        global _requires
+        s = _requires.get(fn.__module__, set())
+        for pkg in pkgs:
+            s.add(pkg)
+        _requires[fn.__module__] = s
+
+        def _require(*args, **kwargs):
+            if exists(pkg):
+                return fn(*args, **kwargs)
+        return _require
+    return _decor
 
 
-class InstallRequires(sublime_plugin.WindowCommand):
-	"""Base class for quick panel to install required pkgs
+class PackageMetaSetRequiresCommand(sublime_plugin.WindowCommand):
+    """WindowCommand to allow external plugins to register dependencies.
 
-	If a plugin uses `packagemeta.requires`, subclassing this
-	WindowCommand will provide a quick panel to list and install
-	missing packages. For example, in the plugin:
+    In cases where an external plugin doesn't want to import PackageMeta directly,
+    dependencies can still be registered via this window command. For example:
 
-	class PluginInstallRequiresCommand(packagemeta.InstallRequires):
-		pass
+    def set_requires():
+        kwargs = {
+            "module": set_requires.__module__,
+            "pkgs": ["PackageA", "PackageB"]
+        }
+        sublime.active_window().run_command("package_meta_set_requires", kwargs)
 
-	And include the following in the plugin's sublime-commands file:
+    See `PackageMetaInstallRequiresCommand` for details on showing a quick panel
+    to install dependencies.
+    """
+    def run(self, module=None, pkgs=[]):
+        log.debug("received module %s", module)
+        log.debug("received pkgs %s", pkgs)
+        global _requires
+        s = _requires.get(module, set())
+        for pkg in pkgs:
+            s.add(pkg)
+        _requires[module] = s
 
-	{
-		"caption": "Plugin: Install Dependencies",
-		"command": "plugin_install_requires"
-	}
 
-	The command will only be visible if a plugin passed to `requires`
-	is not installed.
+class PackageMetaInstallRequiresCommand(sublime_plugin.WindowCommand):
+    """Base class for quick panel to install required pkgs
 
-	"""
+    If a plugin uses `packagemeta.requires`, subclassing this
+    WindowCommand will provide a quick panel to list and install
+    missing packages. For example, in the plugin:
 
-	@property
-	def pkgs(self):
-		global _requires
-		return _requires.get(self.__module__, [])
+    class PluginInstallRequiresCommand(packagemeta.PackageMetaInstallRequiresCommand):
+        def is_visible(self):
+            return self.visible()
 
-	def run(self):
-		if not self.pkgs:
-			return
+    And include the following in the plugin's sublime-commands file:
 
-		self.options = self.get_missing_pkgs()
-		if not self.options:
-			return
+    {
+        "caption": "Plugin: Install Dependencies",
+        "command": "plugin_install_requires"
+    }
 
-		if len(self.options) > 1:
-			self.options.insert(0, "All Packages")
+    The command will only be visible if a plugin passed to `packagemeta.requires`
+    is not installed.
 
-		self.window.show_quick_panel(self.options, self.on_done)
+    If instead you're interacting with packagemeta via `run_command`, you'll first
+    need to declare required packages at an appropriate time for the type of plugin
+    being developed. See `PackageMetaSetRequiresCommand` for details on registering
+    dependencies.
 
-	def on_done(self, picked):
-		if picked == -1:
-			return
+    Once dependencies are registered, create a WindowCommand such as:
 
-		option = self.options[picked]
+    class PluginInstallRequiresCommand(sublime_plugin.WindowCommand):
+        def run(self):
+            self.window.run_command("package_meta_install_requires", {"module": self.__module__})
 
-		if option == "All Packages":
-			for name in self.options[1:]:
-				self.install_pkg(name)
-		else:
-			self.install_pkg(option)
+    Note: In all cases, only packages not installed will be displayed.
+    """
 
-	def get_missing_pkgs(self):
-		p = sublime.packages_path()
-		log.debug("packages_path: %s", p)
-		installed = os.listdir(p)
-		return [pkg for pkg in self.pkgs if pkg not in installed]
+    def run(self, module=None):
+        log.debug("InstallRequiresCommand.run received module %s", module)
+        self.module = module
+        if not self.get_pkgs():
+            return
 
-	def install_pkg(self, name):
-		thread = PackageControl.PackageInstallerThread(PackageControl.PackageManager(), name, None)
-		thread.start()
-		PackageControl.ThreadProgress(thread, 'Installing package %s' % name,
-			'Package %s successfully %s' % (name, "installed"))
+        self.options = self.get_missing_pkgs()
+        if not self.options:
+            return
 
-	def is_visible(self):
-		if not self.get_missing_pkgs():
-			return False
-		return True
+        if len(self.options) > 1:
+            self.options.insert(0, "All Packages")
+
+        self.window.show_quick_panel(self.options, self.on_done)
+
+    def on_done(self, picked):
+        if picked == -1:
+            return
+
+        option = self.options[picked]
+
+        if option == "All Packages":
+            for name in self.options[1:]:
+                self.install_pkg(name)
+        else:
+            self.install_pkg(option)
+
+    def get_pkgs(self):
+        global _requires
+        return _requires.get(self.get_module(), [])
+
+    def get_module(self):
+        if not hasattr(self, "module") or self.module is None:
+            return self.__module__
+        return self.module
+
+    def get_missing_pkgs(self):
+        p = sublime.packages_path()
+        log.debug("packages_path: %s", p)
+        installed = os.listdir(p)
+        return [pkg for pkg in self.get_pkgs() if pkg not in installed]
+
+    def install_pkg(self, name):
+        thread = PackageControl.PackageInstallerThread(PackageControl.PackageManager(), name, None)
+        thread.start()
+        PackageControl.ThreadProgress(thread, 'Installing package %s' % name,
+            'Package %s successfully %s' % (name, "installed"))
+
+    def visible(self):
+        if not self.get_missing_pkgs():
+            return False
+        return True
